cc650f779e011b1bba5f8c92aa8dd399077f6d28
aamgoulart <aamgoulart@outlook.com>
2021-08-17 18:47:05 -0300

    fix bug

--- app/controllers/covid.controller.js
+++ app/controllers/covid.controller.js
@@ -5,7 +5,7 @@ const Covid = db.covid;
 // DONE
 // this endpoint return informations of tse, health and ibge from city, this can be used in page of city
 exports.getAll = (req, res) => {
-   Covid.find()
+   Covid.find().sort({ cases_confirm: 1 })
       .then(data => {
         res.send(data);
       })
